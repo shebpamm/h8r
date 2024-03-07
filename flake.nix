fd47f6d57e9ecd20c0ca082722acf91f0d87a0e4
Erik Karsten <erik@opcode.xyz>
2024-03-07 16:34:06 +0200

    release: 1.1.0

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         in rec {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "h8r";
-            version = "1.0.0";
+            version = "1.1.0";
 
             src = ./.;
             cargoLock = {
