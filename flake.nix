a2fd2244f7c760ded7d0828983eafb3acabc0ba9
Erik Karsten <erik@opcode.xyz>
2025-03-11 15:08:27 +0200

    feat: arm builds

--- flake.nix
+++ flake.nix
@@ -6,23 +6,29 @@
   outputs = { self, nixpkgs, }:
     let
       # Should work with other targets, but not tested.
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+      architectures = {
+        x86_64-linux = "amd64";
+        aarch64-linux = "arm64";
+      };
     in
     {
       packages = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system}.pkgsStatic;
           pkgs-full = nixpkgsFor.${system};
+          arch = builtins.getAttr system architectures;
         in rec {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "h8r";
-            version = "2.0.1";
+            version = "2.1.0";
 
             src = ./.;
             cargoLock = {
@@ -41,7 +47,7 @@
               cat > ./dpkg/DEBIAN/control <<EOF
 Package: h8r
 Version: ${default.version}
-Architecture: amd64
+Architecture: ${arch}
 Maintainer: shebpamm
 Description: h8r
 EOF
