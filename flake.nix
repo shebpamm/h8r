5b5f03f14bace090b30c4cc9317abde0ecc30525
Erik Karsten <erik@opcode.xyz>
2025-06-25 14:06:50 +0300

    feat: use cargo2nix for faster builds

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   description = "Hello world Rust program statically linked against musl";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.12";
+  inputs.nixpkgs.follows = "cargo2nix/nixpkgs";
 
-  outputs = { self, nixpkgs, }:
+  outputs = { self, nixpkgs, cargo2nix }:
     let
       # Should work with other targets, but not tested.
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
@@ -12,7 +13,7 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ cargo2nix.overlays.default ]; });
 
       architectures = {
         x86_64-linux = "amd64";
@@ -25,34 +26,31 @@
           pkgs = nixpkgsFor.${system}.pkgsStatic;
           pkgs-full = nixpkgsFor.${system};
           arch = builtins.getAttr system architectures;
-        in rec {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "h8r";
-            version = "2.2.0";
-
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            rustVersion = "1.83.0";
+            packageFun = import ./Cargo.nix;
           };
+        in
+        rec {
+          default = rustPkgs.workspace.h8r {};
           deb = pkgs.stdenv.mkDerivation {
             name = "h8r";
             phases = [ "installPhase" ];
             installPhase = ''
-              mkdir -p ./dpkg/usr/bin
-              mkdir -p $out
-              cp ${default}/bin/h8r ./dpkg/usr/bin
+                            mkdir -p ./dpkg/usr/bin
+                            mkdir -p $out
+                            cp ${default}/bin/h8r ./dpkg/usr/bin
 
-              mkdir -p ./dpkg/DEBIAN
-              cat > ./dpkg/DEBIAN/control <<EOF
-Package: h8r
-Version: ${default.version}
-Architecture: ${arch}
-Maintainer: shebpamm
-Description: h8r
-EOF
-              ${pkgs-full.dpkg}/bin/dpkg-deb --build ./dpkg
-              mv ./dpkg.deb $out/h8r.deb
+                            mkdir -p ./dpkg/DEBIAN
+                            cat > ./dpkg/DEBIAN/control <<EOF
+              Package: h8r
+              Version: ${default.version}
+              Architecture: ${arch}
+              Maintainer: shebpamm
+              Description: h8r
+              EOF
+                            ${pkgs-full.dpkg}/bin/dpkg-deb --build ./dpkg
+                            mv ./dpkg.deb $out/h8r.deb
             '';
           };
         });
